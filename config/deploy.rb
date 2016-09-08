834f071fd86eea67998423851ab2f15f5ce3c0b8
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-09-08 23:53:46 +0300

    try to fix keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,6 @@ set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-set :ssh_options, {
-  config: false
-}
-
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        true
@@ -25,7 +21,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/semantics/config/provision/keys/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -33,8 +29,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 ## Defaults:
 set :scm,           :git
 set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
+set :format,        :pretty
+set :log_level,     :debug
 set :keep_releases, 1
 
 ## Linked Files & Directories (Default None):
