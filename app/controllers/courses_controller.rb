4821c047268e236ea2376b84827faaf5566df69d
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-09-10 15:32:06 +0300

    #15 add courses controller

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,47 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:edit, :update, :destroy]
+
+  def index
+    @coutses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to courses_url, notice: 'Course was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to courses_url, notice: 'Course was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_url, notice: 'Course was successfully destroyed.'
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :projects, except: :show
     resources :pages, except: :show
     resources :users, except: :show
+    resources :courses, except: :show
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
