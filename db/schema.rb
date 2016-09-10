aa6785122af267cf5c2f0aa821b718954ba479c8
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-09-10 15:11:56 +0300

    #15 add courses model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131171628) do
+ActiveRecord::Schema.define(version: 20160910121117) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string   "name",       null: false
