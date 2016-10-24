82ffeb3bbd05a1745cb72b9e75e5380ec6ae0511
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-10-24 10:23:00 +0300

    #32 add areas from api to index page

--- app/controllers/research_areas_controller.rb
+++ app/controllers/research_areas_controller.rb
@@ -1,5 +1,21 @@
 class ResearchAreasController < ApplicationController
   def index
     @areas = ResearchArea.all.order(:created_at)
+    uris = @areas.map(&:uri)
+    url = 'https://api-lod-itmo-nav-mike.c9users.io/research_areas'
+
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.response :json
+    end
+
+    response = conn.get('find_by_uri', uri: uris)
+    @result = response.body
+  rescue => e
+    @error = '<strong>При запросе к <i>lod.ifmo/REST</i> произошла ошибка!</strong><br /> Обратитесь к администратору для решения проблемы.'
+    logger.tagged('ResearchAreasController', self.class) do
+      logger.error e.message
+      logger.error e.backtrace.join("\n")
+    end
   end
 end
