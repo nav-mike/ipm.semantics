e261a750ca4feec236ab1fce555f8aba0b1ff75e
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-10-24 10:45:14 +0300

    #32 add areas destroy

--- app/controllers/research_areas_controller.rb
+++ app/controllers/research_areas_controller.rb
@@ -1,8 +1,28 @@
 class ResearchAreasController < ApplicationController
+  before_action :set_area, only: :destroy
+
   def index
     @areas = ResearchArea.all.order(:created_at)
+    return if @areas.size <= 0
+    render_areas
+  rescue => e
+    @error = '<strong>При запросе к <i>lod.ifmo/REST</i> произошла ошибка!</strong><br /> Обратитесь к администратору для решения проблемы.'
+    logger.tagged('ResearchAreasController', self.class) do
+      logger.error e.message
+      logger.error e.backtrace.join("\n")
+    end
+  end
+
+  def destroy
+    @area.destroy
+    redirect_to research_areas_url
+  end
+
+  private
+
+  def render_areas
     uris = @areas.map(&:uri)
-    url = 'https://api-lod-itmo-nav-mike.c9users.io/research_areas'
+    url = 'http://api-lod-itmo.herokuapp.com/research_areas'
 
     conn = Faraday.new(url: url) do |faraday|
       faraday.adapter Faraday.default_adapter
@@ -11,11 +31,9 @@ class ResearchAreasController < ApplicationController
 
     response = conn.get('find_by_uri', uri: uris)
     @result = response.body
-  rescue => e
-    @error = '<strong>При запросе к <i>lod.ifmo/REST</i> произошла ошибка!</strong><br /> Обратитесь к администратору для решения проблемы.'
-    logger.tagged('ResearchAreasController', self.class) do
-      logger.error e.message
-      logger.error e.backtrace.join("\n")
-    end
+  end
+
+  def set_area
+    @area = ResearchArea.find params[:id]
   end
 end
--- spec/controllers/research_areas_controller_spec.rb
+++ spec/controllers/research_areas_controller_spec.rb
@@ -5,7 +5,7 @@ describe ResearchAreasController, type: :controller do
   login_user
   render_views
 
-  context 'get #index active' do
+  context 'get #index' do
     subject! { get :index }
 
     it 'should be success' do
@@ -13,10 +13,25 @@ describe ResearchAreasController, type: :controller do
     end
   end
 
+  context 'delete #destroy' do
+    let!(:research_area) { FactoryGirl.create :research_area }
+    subject! { delete :destroy, id: research_area.id }
+
+    it 'should be success' do
+      expect(response).to redirect_to(research_areas_url)
+    end
+  end
+
   context 'routing' do
     it 'get /admin/research_areas' do
       expect(get: '/admin/research_areas').to route_to(
         controller: 'research_areas', action: 'index'
+      )
+    end
+
+    it 'delete /admin/research_areas/1' do
+      expect(delete: '/admin/research_areas/1').to route_to(
+        controller: 'research_areas', action: 'destroy', id: '1'
       )
     end
   end
